eff27e91f0598c13c49871142f39a0c7ea4d7caf
Gevorg Harutyunyan <gevharut@gmail.com>
2021-05-05 23:34:46 +0400

    Setup prettier, tslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    ecmaVersion: 2021,
   },
+  plugins: [],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-        "**/src/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/src/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
       },
     },
   ],
-};
+}
